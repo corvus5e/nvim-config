bedf515e4832cd192ecc504cdfbe134713576906
corvus5e <corvus5e@gmail.com>
2025-01-24 23:39:40 +0100

    Changing gruvbox config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -10,6 +10,9 @@ return {
         -- directly inside the plugin declaration.
         vim.g.gruvbox_material_enable_italic = true
         vim.g.gruvbox_material_background = 'medium'
+		vim.g.gruvbox_material_foreground = 'original'
+		vim.g.gruvbox_material_enable_bold = 1
+		vim.g.gruvbox_material_enable_italic = 1
         vim.cmd.colorscheme('gruvbox-material')
       end
     },
