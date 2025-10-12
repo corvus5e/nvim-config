eaea48f08782d784d96177c98ac2fe79633b482d
corvus5e <corvus5e@gmail.com>
2025-10-12 22:12:42 +0200

    New buff delete, separate files for plugins, neogit horizontal split

---
+++ lua/plugins/neogit.lua
@@ -0,0 +1,17 @@
+return {
+	"NeogitOrg/neogit",
+
+	dependencies = {
+		"nvim-lua/plenary.nvim", -- required
+		"sindrets/diffview.nvim",-- optional - Diff integration
+	},
+
+	config = function()
+		local neogit = require("neogit")
+		neogit.setup {
+			commit_view = {
+				kind = "split"
+			}
+		}
+	end
+}
