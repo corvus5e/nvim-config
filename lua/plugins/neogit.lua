5914e90a1e3903635a81e68a934b70ca2139e981
corvus5e <corvus5e@gmail.com>
2025-10-18 17:51:21 +0200

    Add icons

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -4,6 +4,7 @@ return {
 	dependencies = {
 		"nvim-lua/plenary.nvim", -- required
 		"sindrets/diffview.nvim",-- optional - Diff integration
+		"nvim-tree/nvim-web-devicons",
 	},
 
 	config = function()
