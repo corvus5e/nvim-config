733a694693d8273c26622643a5a2d183c07c8c63
corvus5e <corvus5e@gmail.com>
2026-02-13 00:02:45 +0100

    Setup nvim-dap and nvim-dap-view

--- lua/plugins/nvim-dap-view.lua
+++ lua/plugins/nvim-dap-view.lua
@@ -2,5 +2,13 @@ return {
         "igorlfs/nvim-dap-view",
         ---@module 'dap-view'
         ---@type dapview.Config
-        opts = {},
+        opts = {
+		winbar = {
+        		show = true,
+			sections = { "scopes", "exceptions", "breakpoints", "threads", "repl", "console", "watches"},
+			default_section = "scopes",
+		},
+
+		auto_toggle = true,
+	},
 }
--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -3,25 +3,49 @@ return {
 
 	config = function()
 		local dap = require("dap")
+
+		dap.defaults.fallback.terminal_win_cmd = '50vsplit new'
+
+		vim.fn.sign_define('DapBreakpoint', {text='', texthl='DiagnosticError', linehl='', numhl='DiagnosticError'})
+		vim.fn.sign_define('DapBreakpointCondition', {text='', texthl='DiagnosticError', linehl='', numhl='DiagnosticError'})
+		vim.fn.sign_define('DapLogPoint', {text='', texthl='DiagnosticError', linehl='', numhl='DiagnosticError'})
+		vim.fn.sign_define('DapStopped', {text='󰁕', texthl='DiagnosticInfo', linehl='Visual', numhl='DiagnosticInfo'})
+		vim.fn.sign_define('DapBreakpointRejected', {text='', texthl='DiagnosticHint', linehl='', numhl='DiagnosticHint'})
+
+		--vim.keymap.set('n', '<F5>', ':DapNew<CR>', { silent = true, desc = 'Dap New' })
+		vim.keymap.set('n', '<leader>tb', ':DapToggleBreakpoint<CR>', { silent = true, desc = '[T]oggle [B]reakpoint' })
+		vim.keymap.set('n', '<leader>tv', ':DapViewToggle<CR>', { silent = true, desc = '[T]oggle [V]iew' })
+
+		-- Let's give our listener a clear name
+		local listener_id = "user_keymaps"
+		
+		-- 1. Setup listeners to turn mappings ON
+		dap.listeners.after.event_initialized[listener_id] = function()
+		  -- These will only exist while debugging
+		  vim.keymap.set('n', '<Down>', dap.step_over, { desc = 'Step Over' })
+		  vim.keymap.set('n', '<Right>', dap.step_into, { desc = 'Step Into' })
+		  vim.keymap.set('n', '<Left>', dap.step_out, { desc = 'Step Out' })
+		  vim.keymap.set('n', '<Up>', dap.continue, { desc = 'Continue' })
+		  print("Debugger Active: Arrow keys remapped to Stepping")
+		end
+		
+		-- 2. Setup listeners to turn mappings OFF
+		local function clear_keys()
+		  pcall(vim.keymap.del, 'n', '<Down>')
+		  pcall(vim.keymap.del, 'n', '<Right>')
+		  pcall(vim.keymap.del, 'n', '<Left>')
+		  pcall(vim.keymap.del, 'n', '<Up>')
+		  print("Debugger Inactive: Arrow keys restored")
+		end
+		
+		-- We trigger the cleanup on both 'terminated' and 'exited'
+		dap.listeners.before.event_terminated[listener_id] = clear_keys
+		dap.listeners.before.event_exited[listener_id] = clear_keys
+
 		dap.adapters.lldb = {
 			type = "executable",
 			command = "/usr/bin/lldb-dap", -- adjust as needed, must be absolute path
 			name = "lldb"
 		}
-		dap.configurations.cpp = {
-			{
-				name = 'Launch',
-				type = 'lldb',
-				request = 'launch',
-				program = function()
-					return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-				end,
-				cwd = '${workspaceFolder}',
-				stopOnEntry = false,
-				args = {},
-			}
-		}
-
-		dap.configurations.c = dap.configurations.cpp
 	end
 }
